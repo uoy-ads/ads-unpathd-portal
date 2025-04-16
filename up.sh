f130365a26e5ae7f1b95ea9945b5e833b109ff24
Jamie Bradley <99401546+adsjim@users.noreply.github.com>
2025-04-16 11:03:11 +0100

    Update up.sh

--- up.sh
+++ up.sh
@@ -1,6 +1,2 @@
 #! /bin/bash
-if [ "$HOSTNAME" = ads24unpathd0 ]; then
-    docker compose -f docker-compose.production.yml up --build -d
-else
-    docker compose -f docker-compose.demo.yml up --build -d
-fi
+docker compose -f docker-compose.yml up --build -d
