349f37ddc3fc0d1c1bd77ac444d695cb3135916b
Jamie Bradley <99401546+adsjim@users.noreply.github.com>
2025-04-16 10:39:21 +0100

    Remove sudo from docker commands

--- up.sh
+++ up.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 if [ "$HOSTNAME" = ads24unpathd0 ]; then
-    sudo docker compose -f docker-compose.production.yml up --build -d
+    docker compose -f docker-compose.production.yml up --build -d
 else
-    sudo docker compose -f docker-compose.demo.yml up --build -d
+    docker compose -f docker-compose.demo.yml up --build -d
 fi
